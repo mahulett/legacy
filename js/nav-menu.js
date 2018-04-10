33dd5306709e559a517e4efb06ad3a766817dca5
Marguerite Hulett <mhulett@movingtargets.com>
2018-04-10 16:14:36 -0400

    Finished styling nav callout slide, updated header on all pages

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -1,13 +1,18 @@
 function openCalloutMenu() {
-    var navCallout = document.querySelector(".nav-callout-menu");
-    navCallout.classList.toggle("nav-callout-active");
+  var navCallout = document.querySelector(".nav-callout-menu");
+  navCallout.classList.toggle("nav-callout-active");
+}
+
+function openCalloutSlide() {
+  var navCallout = document.querySelector(".nav-callout-slide");
+  navCallout.classList.toggle("nav-callout-active");
 }
 
 function openNavMenu() {
-    var navMenu = document.querySelector(".nav-links");
-    navMenu.classList.toggle("nav-open");
-    var navIcon = document.querySelector(".nav-icon");
-    navIcon.classList.toggle("nav-close");
+  var navMenu = document.querySelector(".nav-links");
+  navMenu.classList.toggle("nav-open");
+  var navIcon = document.querySelector(".nav-icon");
+  navIcon.classList.toggle("nav-close");
 }
 
 function openNavSubMenu() {
